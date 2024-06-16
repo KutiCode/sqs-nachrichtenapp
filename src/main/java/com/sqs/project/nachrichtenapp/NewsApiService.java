180401e51677e9ae91fc37a505fe1b0ac5dec3d6
kuticode <kutay2408@gmail.com>
2024-06-16 05:30:47 +0200

    Started to implement backend fetch logic

--- src/main/java/com/sqs/project/nachrichtenapp/NewsApiService.java
+++ src/main/java/com/sqs/project/nachrichtenapp/NewsApiService.java
@@ -1,22 +1,37 @@
 package com.sqs.project.nachrichtenapp;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class NewsApiService {
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
-    @Value("${newsapi.key}")
-    private static String API_KEY;
+    private final String API_KEY = "bd07d53bf22d404bbc22dbefe92997e0";
+    private final String BASE_URL = "https://newsapi.org/v2/top-headlines?country=";
 
-    private static final String URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY ;
 
-    public String fetchNews() {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(URL, String.class);
-        return response.getBody();
+    public String fetchNews(String country, String pageSize) {
+        String newsData = redisTemplate.opsForValue().get("newsData");
+        if (newsData == null) {
+            RestTemplate restTemplate = new RestTemplate();
+            String URL = BASE_URL + country + "&pageSize=" + pageSize + "&apiKey=" + API_KEY;
+            ResponseEntity<String> response = restTemplate.getForEntity(URL, String.class);
+            newsData = response.getBody();
+            saveNewsData(newsData);
+        }
+        return newsData;
+    }
+
+    private void saveNewsData(String newsData) {
+        redisTemplate.opsForValue().set("newsData", newsData);
+        redisTemplate.expire("newsData", 1, TimeUnit.HOURS);
     }
 }
--- src/main/java/com/sqs/project/nachrichtenapp/NewsController.java
+++ src/main/java/com/sqs/project/nachrichtenapp/NewsController.java
@@ -1,7 +1,8 @@
 package com.sqs.project.nachrichtenapp;
 
-
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,8 @@ public class NewsController {
         this.newsApiService = newsApiService;
     }
 
-
+    @GetMapping("/news/{country}/{pageSize}")
+    public String getNews(@PathVariable String country, @PathVariable String pageSize) {
+        return newsApiService.fetchNews(country, pageSize);
+    }
 }
