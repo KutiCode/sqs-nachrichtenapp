eeef51e09283631ecfd6eb13391dcafc8b77ade2
kuticode <kutay2408@gmail.com>
2024-06-21 00:36:54 +0200

    created Integrationtest for NewsController

--- backend/src/test/java/com/sqs/project/nachrichtenapp/NewsControllerIntegrationTests.java
+++ backend/src/test/java/com/sqs/project/nachrichtenapp/NewsControllerIntegrationTests.java
@@ -1,47 +1,81 @@
 package com.sqs.project.nachrichtenapp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import java.io.IOException;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class NewsControllerIntegrationTests {
+import static org.mockito.Mockito.lenient;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class NewsControllerIntegrationTests {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+
+    @Mock
+    private NewsApiService newsApiService;
+
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        NewsResponse trendNewsResponse = new NewsResponse();
+        NewsResponse specificNewsResponse = new NewsResponse();
+        // Mocking the service methods leniently
+        lenient().when(newsApiService.fetchTrendNews("us", "2024-06-18")).thenReturn(trendNewsResponse);
+        lenient().when(newsApiService.fetchSpecificNews("keyword")).thenReturn(specificNewsResponse);
+    }
 
     @Test
     void testGetNews() throws Exception {
-        String url = "http://localhost:8080/news/us/2024-06-18";
-        NewsResponse mockResponse = new NewsResponse();
-        // Set properties of mockResponse as needed
+        mockMvc.perform(get("/news/us/2024-06-18")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("ok"))
+                .andExpect(jsonPath("$.totalResults").exists())
+                .andExpect(jsonPath("$.articles").isArray())
+                .andExpect(jsonPath("$.articles[0].author").exists())
+                .andExpect(jsonPath("$.articles[0].title").exists())
+                .andExpect(jsonPath("$.articles[0].description").exists())
+                .andExpect(jsonPath("$.articles[0].url").exists())
+                .andExpect(jsonPath("$.articles[0].urlToImage").exists())
+                .andExpect(jsonPath("$.articles[0].publishedAt").exists())
+                .andExpect(jsonPath("$.articles[0].content").exists());
+    }
 
-        String mockResponseJson = objectMapper.writeValueAsString(mockResponse);
-
-        MockRestServiceServer mockServer = MockRestServiceServer.createServer(restTemplate);
-        mockServer.expect(requestTo(url))
-                .andRespond(withSuccess(mockResponseJson, MediaType.APPLICATION_JSON));
-
-        ResponseEntity<NewsResponse> response = restTemplate.getForEntity(url, NewsResponse.class);
-
-        // Verwenden Sie getStatusCode und value anstelle von getStatusCodeValue
-        int statusCode = response.getStatusCode().value();
-        assertEquals(200, statusCode);
-
-        assertNotNull(response.getBody());
-        // Additional assertions to verify the content of the response body
+    @Test
+    void testSearchForSpecificNews() throws Exception {
+        mockMvc.perform(get("/news/search/keyword")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("ok"))
+                .andExpect(jsonPath("$.totalResults").exists())
+                .andExpect(jsonPath("$.articles").isArray())
+                .andExpect(jsonPath("$.articles[0].author").exists())
+                .andExpect(jsonPath("$.articles[0].title").exists())
+                .andExpect(jsonPath("$.articles[0].description").exists())
+                .andExpect(jsonPath("$.articles[0].url").exists())
+                .andExpect(jsonPath("$.articles[0].urlToImage").exists())
+                .andExpect(jsonPath("$.articles[0].publishedAt").exists())
+                .andExpect(jsonPath("$.articles[0].content").exists());
     }
 }
