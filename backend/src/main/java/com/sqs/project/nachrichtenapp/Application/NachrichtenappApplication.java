7ce7b879ea79cceb120fc7bf95eaa8abe265f7da
kuticode <kutay2408@gmail.com>
2024-06-22 00:58:40 +0200

    fixed backend connection issues

--- backend/src/main/java/com/sqs/project/nachrichtenapp/Application/NachrichtenappApplication.java
+++ backend/src/main/java/com/sqs/project/nachrichtenapp/Application/NachrichtenappApplication.java
@@ -3,9 +3,11 @@ package com.sqs.project.nachrichtenapp.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.sqs.project.nachrichtenapp")
 public class NachrichtenappApplication {
 
 	public static void main(String[] args) {
